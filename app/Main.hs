f287f4ca5b97a78d4141bb90aa6208058a94f6bb
robert0004 <6243646+robert0004@users.noreply.github.com>
2024-04-25 07:50:47 -0700

    fix

--- app/Main.hs
+++ app/Main.hs
@@ -13,26 +13,70 @@ import Data.Maybe
 import Control.Applicative
 
 type Label = [Char]
-data Lambda = Var Label | Apply Lambda Lambda | Lam Label Lambda
+data Lambda = Var Label | Apply Lambda Lambda | Lam Label Lambda deriving Show
 
-subst :: Label -> Lambda -> Lambda -> Lambda 
-subst var what expr = case expr of 
-                        Var x -> if x == var then what else Var x 
-                        Apply e1 e2 -> Apply (subst var what e1) (subst var what e2)
-                        Lam x e -> if x == var then Lam x e else Lam x (subst var what e) 
+instance Semigroup Lambda where 
+    (<>) = Apply
+
+instance Monoid Lambda where 
+    mempty = identityLambda
+
+identityLambda :: Lambda 
+identityLambda = Lam "x" (Var "x")
+
+trueLambda :: Lambda
+trueLambda = Lam "x" (Lam "y" (Var "x"))
+
+falseLambda :: Lambda
+falseLambda = Lam "x" (Lam "y" (Var "y"))
+
+andLambda :: Lambda
+andLambda = Lam "p" (Lam "q" (Apply (Apply (Var "p") (Var "q")) (Var "p")))
+
+orLambda :: Lambda
+orLambda = Lam "p" (Lam "q" (Apply (Apply (Var "p") (Var "p")) (Var "q")))
+
+notLambda :: Lambda
+notLambda = Lam "p" (Apply (Apply (Var "p") falseLambda) trueLambda)
+
+newVar :: Lambda -> Label -> Label
+newVar expr var = head $ filter (`notElem` freeVars expr) (fmap (var ++) (fmap show [1..]))
+
+freeVars :: Lambda -> [Label]
+freeVars (Var x)       = [x]
+freeVars (Lam x e)     = freeVars e \\ [x]
+freeVars (Apply e1 e2) = freeVars e1 `union` freeVars e2
+
+
+alphaConvert :: Label -> Lambda -> Lambda -> Lambda
+alphaConvert x what expr = case expr of
+                                Lam y e | x /= y && y `elem` freeVars what ->
+                                    let y' = newVar expr y
+                                    in Lam y' (alphaConvert x what (subst y (Var y') e))
+                                _ -> expr
+
+subst :: Label -> Lambda -> Lambda -> Lambda
+subst var what expr = case alphaConvert var what expr of
+                           Var x -> if x == var then what else Var x
+                           Apply e1 e2 -> Apply (subst var what e1) (subst var what e2)
+                           Lam x e -> if x == var then Lam x e else Lam x (subst var what e)
 
 betaReduce :: Lambda -> Maybe Lambda
-betaReduce expr = case expr of 
-                    Apply (Lam x e1) e2 -> Just $ subst x e2 e1
-                    Apply e1 e2 -> Apply <$> betaReduce e1 <*> pure e2 <|> Apply e1 <$> betaReduce e2
-                    Lam x e -> Lam x <$> betaReduce e 
-                    _ -> Nothing
+betaReduce expr = case expr of
+                       Apply (Lam x e1) e2 -> Just $ subst x e2 e1
+                       Apply e1 e2 ->
+                            case betaReduce e1 of
+                                 Just e1' -> Just $ Apply e1' e2
+                                 Nothing -> Apply e1 <$> betaReduce e2
+                       Lam x e -> Lam x <$> betaReduce e
+                       _ -> Nothing
 
--- runs beta reduction + alpha conversion
+
 lambdaReduce :: Lambda -> Lambda
-lambdaReduce expr = fromMaybe expr $ do 
-    reduced <- betaReduce expr 
-    return $ lambdaReduce reduced
+lambdaReduce expr = case betaReduce expr of
+                         Just expr' -> lambdaReduce expr'
+                         Nothing -> expr
+
 
 
 eitherToMaybeTM :: Monad m => m (Either b a) -> MaybeT m a 
